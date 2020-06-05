f58e58c89a13e28eb2e93108049ca3bcf4aab02d
TildaDares <mathildaudufo@gmail.com>
2020-06-05 05:45:51 +0100

    Completed  the exercise

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,15 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-def substrings do
-
+def substrings(string = "Put something in there", dictionary)  
+    dictionary_hash = {}
+dictionary.each do |word|
+    if /#{word}/i =~ string
+        dictionary_hash[word] = string.scan(/#{word}/i).length
+    end
 end
+puts dictionary_hash
+end
+
+puts "Enter a string"
+string = gets.chomp
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings(string, dictionary)
