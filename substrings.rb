41290ab2ec78b867dfb0249fa1a9ece13ee8982f
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-01 12:50:23 +0100

    Refactored

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,15 @@
-def substrings(string = "Put something in there", dictionary)  
-    dictionary_hash = {}
-dictionary.each do |word|
+def substrings(string = 'Put something in there', dictionary)  
+  dictionary_hash = {}
+  dictionary.each do |word|
     if /#{word}/i =~ string
-        dictionary_hash[word] = string.scan(/#{word}/i).length
+      dictionary_hash[word] = string.scan(/#{word}/i).length
     end
-end
-puts dictionary_hash
+  end
+  puts dictionary_hash
 end
 
-puts "Enter a string"
+puts 'Enter a string'
 string = gets.chomp
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ['below', 'down', 'go', 'going', 'horn', 'how', 'howdy', 'it', 'i', 'low', 'own', 'par', 'partner', 'sit']
 
 substrings(string, dictionary)
